a6fbe7d04596b772b9f7031fdf887e0be02fe2b3
Aarushi Rawat <aarushirawat43@gmail.com>
2025-08-31 11:47:37 +0530

    config file

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: "0.0.0.0",                // listen on all addresses, required for Render
+    port: process.env.PORT || 5173, // use Render's assigned port, fallback to 5173 locally
+  },
 })
